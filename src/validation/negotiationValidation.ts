a64bb776fcced9cf727daa339f2048a3862ee9ff
protonsDev <shubham.joshi@protonshub.in>
2025-04-04 14:13:45 +0530

    fixed db enum

--- src/validation/negotiationValidation.ts
+++ src/validation/negotiationValidation.ts
@@ -4,7 +4,7 @@ import Joi from "joi";
 export const negotiationSchema = Joi.object({
   quoteId: Joi.number().integer().required(), 
   proposedCost: Joi.number().precision(2).positive().required(), 
-  proposedBy: Joi.string().required().valid('buyer','seller'),
+  proposedBy: Joi.string().required().valid('buyer','supplier'),
   status:Joi.string().required().valid("pending"),
   message: Joi.string().required(),
 
