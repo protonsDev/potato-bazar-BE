5d4a0900642cf1646039130db834ae11c1936414
protonsDev <shubham.joshi@protonshub.in>
2025-04-10 16:43:39 +0530

    added user profile api

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,12 +2,14 @@ import { createValidator } from "express-joi-validation";
 import express from "express";
 import { userSchema } from "../validation/userValidation";
 import { signup, login, getUserProfile, getSellerList } from "../controller/user";
+import { authMiddleware } from "../utlis/userAuth";
 const router = express.Router();
 const validator = createValidator({});
 
 router.post("/signup", validator.body(userSchema), signup);
 router.post("/login", login);
 router.get("/seller-list", getSellerList);
+router.get("/user-profile", authMiddleware,getUserProfile);
 
 
 export default router;
